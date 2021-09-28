9257de596a950551e3d65c18669d00a7ecb18c52
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-28 10:26:34 +0200

    refactor: export json instead of class

--- src/ServalBuildInformation.ts
+++ src/ServalBuildInformation.ts
@@ -1,18 +1,7 @@
-export class ServalBuildInformation {
-    public readonly version: string = "{{version}}"
-    public readonly build: string = "{{build}}"
-    public readonly date: string = "{{date}}"
-    public readonly commit: string = "{{commit}}"
-    public readonly dependencies: object
-
-    private constructor() {
-        try {
-            // eslint-disable-next-line @typescript-eslint/quotes
-            this.dependencies = JSON.parse(`{{dependencies}}`)
-        } catch (e) {
-            this.dependencies = {}
-        }
-    }
-
-    public static readonly info: ServalBuildInformation = new ServalBuildInformation()
+export default {
+    version: "{{version}}",
+    build: "{{build}}",
+    date: "{{date}}",
+    commit: "{{commit}}",
+    dependencies: "{{dependencies}}"
 }
