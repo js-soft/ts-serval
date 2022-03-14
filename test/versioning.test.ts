21834d3d5f7b36c94d51ddcf9fb23ece5b25f529
tnotheis <timo.notheisen@js-soft.com>
2022-03-14 14:22:09 +0100

    chore: disable eslint errors

--- test/versioning.test.ts
+++ test/versioning.test.ts
@@ -18,7 +18,7 @@ class VersionedClassV2 extends Serializable {
 export class VersioningTest {
     public static init(): void {
         describe("VersionedClassV1", function () {
-            const json = { "@type": "VersionedClass", propOld: "val" }
+            const json = { "@type": "VersionedClass", propOld: "val" } // eslint-disable-line @typescript-eslint/naming-convention
 
             it("fromT", function () {
                 const obj = VersionedClassV1.fromT<VersionedClassV1>(json, VersionedClassV1)
@@ -27,7 +27,7 @@ export class VersioningTest {
         })
 
         describe("VersionedClassV2", function () {
-            const json = { "@type": "VersionedClass", "@version": "2", propNew: "val" }
+            const json = { "@type": "VersionedClass", "@version": "2", propNew: "val" } // eslint-disable-line @typescript-eslint/naming-convention
 
             it("fromT", function () {
                 const obj = VersionedClassV2.fromT<VersionedClassV2>(json, VersionedClassV2)
@@ -36,7 +36,7 @@ export class VersioningTest {
 
             // When the value of "@version" is an object instead of a string (as the original error message suggests), the resulting error message is different
             it("fromT with version as object", function () {
-                const json = { "@type": "VersionedClass", "@version": { version: "2" }, propNew: "val" }
+                const json = { "@type": "VersionedClass", "@version": { version: "2" }, propNew: "val" } // eslint-disable-line @typescript-eslint/naming-convention
                 const obj = VersionedClassV2.fromT<VersionedClassV2>(json, VersionedClassV2)
                 expect((obj.toJSON() as any)["@version"]).to.equal("2")
             })
