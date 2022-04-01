e3aa9a3478831e407f4a692be96952b2e7981b4a
stnmtz <40369439+stnmtz@users.noreply.github.com>
2022-04-01 13:51:28 +0200

    test: fix versioning test

--- test/versioning.test.ts
+++ test/versioning.test.ts
@@ -1,44 +1,117 @@
 import { Serializable, serialize, type, validate } from "@js-soft/ts-serval"
 import { expect } from "chai"
+import { expectThrows } from "./testUtil"
 
 @type("VersionedClass")
 class VersionedClassV1 extends Serializable {
     @serialize()
     @validate()
     public propOld: string
+
+    public static from(value: any): VersionedClassV1 {
+        return Serializable.fromT<VersionedClassV1>(value, VersionedClassV1)
+    }
 }
 
-@type("VersionedClass", "2")
+@type("VersionedClass", { version: 2 })
 class VersionedClassV2 extends Serializable {
     @serialize()
     @validate()
     public propNew: string
+
+    public static from(value: any): VersionedClassV2 {
+        return Serializable.fromT<VersionedClassV2>(value, VersionedClassV2)
+    }
 }
 
 export class VersioningTest {
     public static init(): void {
         describe("VersionedClassV1", function () {
-            const json = { "@type": "VersionedClass", propOld: "val" } // eslint-disable-line @typescript-eslint/naming-convention
+            const json = { "@type": "VersionedClass", propOld: "valOld" } // eslint-disable-line @typescript-eslint/naming-convention
+
+            it("fromUnknown", function () {
+                const obj = Serializable.fromUnknown(json) as VersionedClassV1
+                expect(obj).instanceOf(VersionedClassV1)
+                expect(obj.propOld).to.equal("valOld")
+                expect((obj.toJSON() as any)["@version"]).to.be.undefined
+            })
 
             it("fromT", function () {
                 const obj = VersionedClassV1.fromT<VersionedClassV1>(json, VersionedClassV1)
+                expect(obj).instanceOf(VersionedClassV1)
+                expect(obj.propOld).to.equal("valOld")
                 expect((obj.toJSON() as any)["@version"]).to.be.undefined
+            })
+
+            it("from", function () {
+                const obj = VersionedClassV1.from(json)
+                expect(obj).instanceOf(VersionedClassV1)
+                expect(obj.propOld).to.equal("valOld")
+                expect((obj.toJSON() as any)["@version"]).to.be.undefined
+            })
+
+            it("from throwsErrorOnWrongVersion", function () {
+                expectThrows(() => {
+                    VersionedClassV2.from(json)
+                }, "VersionedClassV2.propNew :: Value is not defined")
+            })
+
+            it("fromT throwsErrorOnWrongVersion", function () {
+                expectThrows(() => {
+                    VersionedClassV2.fromT<VersionedClassV2>(json, VersionedClassV2)
+                }, "VersionedClassV2.propNew :: Value is not defined")
+            })
+
+            it("fromUnknown throwsErrorOnWrongVersion", function () {
+                const wrongJSON = { "@type": "VersionedClass", propNew: "valNew" } // eslint-disable-line @typescript-eslint/naming-convention
+
+                expectThrows(() => {
+                    Serializable.fromUnknown(wrongJSON) as VersionedClassV2
+                }, "VersionedClassV1.propOld :: Value is not defined")
             })
         })
 
         describe("VersionedClassV2", function () {
-            const json = { "@type": "VersionedClass", "@version": "2", propNew: "val" } // eslint-disable-line @typescript-eslint/naming-convention
+            const json = { "@type": "VersionedClass", "@version": 2, propNew: "valNew" } // eslint-disable-line @typescript-eslint/naming-convention
+
+            it("fromUnknown", function () {
+                const obj = Serializable.fromUnknown(json) as VersionedClassV2
+                expect(obj).instanceOf(VersionedClassV2)
+                expect(obj.propNew).to.equal("valNew")
+            })
 
             it("fromT", function () {
                 const obj = VersionedClassV2.fromT<VersionedClassV2>(json, VersionedClassV2)
-                expect((obj.toJSON() as any)["@version"]).to.equal("2")
+                expect(obj).instanceOf(VersionedClassV2)
+                expect(obj.propNew).to.equal("valNew")
+                expect((obj.toJSON() as any)["@version"]).to.equal(2)
             })
 
-            // When the value of "@version" is an object instead of a string (as the original error message suggests), the resulting error message is different
-            it("fromT with version as object", function () {
-                const json = { "@type": "VersionedClass", "@version": { version: "2" }, propNew: "val" } // eslint-disable-line @typescript-eslint/naming-convention
-                const obj = VersionedClassV2.fromT<VersionedClassV2>(json, VersionedClassV2)
-                expect((obj.toJSON() as any)["@version"]).to.equal("2")
+            it("from", function () {
+                const obj = VersionedClassV2.from(json)
+                expect(obj).instanceOf(VersionedClassV2)
+                expect(obj.propNew).to.equal("valNew")
+                expect((obj.toJSON() as any)["@version"]).to.equal(2)
+            })
+
+            it("from throwsErrorOnWrongVersion", function () {
+                expectThrows(() => {
+                    VersionedClassV1.from(json)
+                }, "VersionedClassV1.propOld :: Value is not defined")
+            })
+
+            it("fromT throwsErrorOnWrongVersion", function () {
+                expectThrows(() => {
+                    VersionedClassV1.fromT<VersionedClassV1>(json, VersionedClassV1)
+                }, "VersionedClassV1.propOld :: Value is not defined")
+            })
+
+            it("fromUnknown throwsErrorOnWrongVersion", function () {
+                const wrongJSON = { "@type": "VersionedClass", "@version": 2, propOld: "valOld" } // eslint-disable-line @typescript-eslint/naming-convention
+
+                expectThrows(() => {
+                    Serializable.fromUnknown(wrongJSON) as VersionedClassV2
+                }, "VersionedClassV2.propNew :: Value is not defined")
             })
         })
     }
