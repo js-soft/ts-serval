51db4be840222972c95427b8c5ae8cce5f2030ec
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-09-12 11:54:34 +0200

    chore: add overrides

--- src/errors.ts
+++ src/errors.ts
@@ -4,7 +4,7 @@ export class SerializationError extends ServalError {
     public type: string
     public property: string
     public reason: string
-    public cause: Error | undefined
+    public override cause: Error | undefined
 
     public constructor(type: string, property: string, reason: string, cause?: Error) {
         super(`${type}.${property} :: ${reason}`)
