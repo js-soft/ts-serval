4c965c1ad05a04f6ec79403d61bdc7fc8b6bc784
Timo Notheisen <timo.notheisen@js-soft.com>
2026-03-23 09:52:44 +0100

    chore: ignore vulnerabilities

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,5 +5,5 @@ npm ci
 npm run lint:prettier
 npm run lint:eslint
 npx license-check
-npx better-npm-audit audit --exclude 1112706
+npx better-npm-audit audit --exclude 1112706,1113686
 npm run build:ci
